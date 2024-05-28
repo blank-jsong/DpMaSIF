cdd3e8e53dcb3d7f00a50583d953988b834bf0fb
blank-jsong <844909660@qq.com>
2024-05-28 19:11:32 +0800

    Add files via upload

---
+++ data/train_model_dpmasif.sh
@@ -0,0 +1,8 @@
+export PYTHONPATH=$PYTHONPATH:$MONET_DIR:./
+masif_root=/mobile_disk/masif
+masif_source=$masif_root/source/
+masif_matlab=$masif_root/source/matlab_libs/
+export PYTHONPATH=$PYTHONPATH:$masif_source
+source $masif_root/set_up.sh
+
+python -u $masif_source/masif_ligand/dpmasif_train.py
